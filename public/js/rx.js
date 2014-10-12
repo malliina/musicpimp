4c07cdce801157df2359e891f1a38f2bbfbc56e0
malliina <malliina123@gmail.com>
2014-10-12 13:15:42 +0300

    fb

--- public/js/rx.js
+++ public/js/rx.js
@@ -4,6 +4,7 @@ var tableContent;
 
 var onconnect = function (payload) {
     webSocket.send(JSON.stringify({cmd: "subscribe"}));
+    setFeedback("Connected.");
 };
 var onmessage = function (payload) {
     var event = jQuery.parseJSON(payload.data);
@@ -25,6 +26,15 @@ var prepend = function (e) {
         trc = "";
     }
     tableContent.prepend("<tr class=" + trc + "><td class='col-md-1'>" + e.timeFormatted + "</td><td>" + e.message + "</td><td>" + e.loggerName + "</td><td>" + e.threadName + "</td><td>" + e.level + "</td></tr>")
+};
+var onclose = function (payload) {
+    setFeedback("Connection closed.");
+};
+var onerror = function (payload) {
+    setFeedback("Connection error.");
+};
+var setFeedback = function (fb) {
+    $('#status').html(fb);
 };
 $(document).ready(function () {
     tableContent = $("#logTableBody");
