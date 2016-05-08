30533775e25f9c65a3f3e4c8b4a31822873937e3
Michael Skogberg <michael.skogberg@outlook.com>
2016-05-08 20:02:51 +0300

    3.0.3

--- project/PimpBuild.scala
+++ project/PimpBuild.scala
@@ -37,7 +37,7 @@ object PimpBuild extends Build {
 
   lazy val commonSettings = PlayProject.assetSettings ++ Seq(
     javaOptions ++= Seq("-Dorg.slf4j.simpleLogger.defaultLogLevel=error"),
-    version := "3.0.2",
+    version := "3.0.3",
     organization := "org.musicpimp",
     scalaVersion := "2.11.7",
     retrieveManaged := false,
