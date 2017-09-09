336f6b3db21a1b932a9f6a165ca1dcc51d8a3ca1
Michael Skogberg <michael.skogberg@futurice.com>
2017-09-09 17:10:44 +0300

    Fix JSON API regression

--- musicpimp/app/com/malliina/musicpimp/stats/RecentEntry.scala
+++ musicpimp/app/com/malliina/musicpimp/stats/RecentEntry.scala
@@ -5,13 +5,24 @@ import java.time.Instant
 import java.util.Date
 
 import com.malliina.musicpimp.audio.TrackMeta
-import play.api.libs.json.{Format, Json}
+import play.api.libs.json.{Format, Json, Writes}
 
-case class RecentEntry(track: TrackMeta, when: Instant) extends TopEntry {
-  val whenFormatted = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Date.from(when))
+case class RecentEntry(track: TrackMeta, timestamp: Instant) extends TopEntry {
+  val whenFormatted = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Date.from(timestamp))
+
+  def whenMillis = timestamp.toEpochMilli
 }
 
 object RecentEntry {
-  implicit def json(implicit f: Format[TrackMeta]): Format[RecentEntry] =
-    Json.format[RecentEntry]
+  val When = "when"
+  val WhenFormatted = "whenFormatted"
+
+  implicit def json(implicit f: Format[TrackMeta]): Format[RecentEntry] = {
+    val base = Json.format[RecentEntry]
+    val writer = Writes[RecentEntry] { r =>
+      val extras = Json.obj(When -> r.whenMillis, WhenFormatted -> r.whenFormatted)
+      base.writes(r) ++ extras
+    }
+    Format(base, writer)
+  }
 }
